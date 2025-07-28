205ce58a3424e52ec2df24eabab3cc8de12c81ab
AmanAyoub <aman.ayoub16@gmail.com>
2025-07-28 08:40:26 +0430

    myLogger middleware function

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+}
+
+app.use(myLogger);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
