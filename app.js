7b1b496a666bbf56b3f21573a12ff8d796989cc3
AmanAyoub <aman.ayoub16@gmail.com>
2025-07-28 08:57:53 +0430

    request time

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const myLogger = function (req, res, next) {
   next();
 }
 
+const requestTime = function (req, res, next) {
+  req.requestTime = (new Date()).toString();
+  next();
+}
+
 app.use(myLogger);
+app.use(requestTime);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let responseText = 'Hello World!<br>';
+  responseText += `<small>Requested at: ${req.requestTime}</small>`;
+  res.send(responseText);
 });
 
 app.listen(PORT, () => {
